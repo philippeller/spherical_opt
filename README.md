fd7693074d78c19affd61c9a95c52fd18c55f0a5
Philipp Eller <peller@phys.ethz.ch>
2019-04-26 17:29:29 -0400

    making prokect installable

---
+++ README.md
@@ -0,0 +1,7 @@
+# Apherical_opt
+
+Module providing spherically aware optimization routines
+
+## Installation
+
+`pip install --editable .`
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='spherical_opt',
+    version='0.1dev',
+    packages=['spherical_opt',],
+    license='Apache 2.0',
+    author='Philipp Eller',
+    long_description=open('README.md').read(),
+
+    setup_requires=[
+        'pip>=1.8',
+        'setuptools>18.5',
+        'numpy>=1.11'
+    ],
+)
